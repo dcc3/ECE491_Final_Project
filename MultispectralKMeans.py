de2351a27173d0c55b82c7948b4d7a3dab447fdf
David <dcc3@users.noreply.github.com>
2019-04-20 14:54:44 -0400

    Gonna delete some shit

--- MultispectralKMeans.py
+++ MultispectralKMeans.py
@@ -1,13 +1,58 @@
 import sklearn
+import cv2
 import numpy as np
 from sklearn.cluster import KMeans
 from matplotlib import pyplot as plt
 
 
+class MultispectralKMeans:
+	
+	def __init__(self):
+		pass
+	
+	# Wraps SKLearn library's KMeans function for use in our project
+	def calculateLabels(self, k_clusters, data):
+	
+		# Set the number of clusters
+		kmeans = KMeans(n_clusters=k_clusters)
+
+		# Fit data to number of clusters
+		kmeans = kmeans.fit(data)
+
+		# Determine labels from fitting
+		labels = kmeans.predict(data)
+		
+		# Coordinates of clusters
+		centroids = kmeans.cluster_centers_
+	
+	
+		return labels, centroids
 
 
-
-
+	def markupImage(self, rgbImg, labels):
+	
+		# Set to upper limit of 50 labels
+		label_list = len(np.unique(labels))
+		blue = [0]*label_list
+		green = [0]*label_list
+		red = [0]*label_list
+		
+		# assign random color for each label
+		for lab in range(0,label_list):
+			blue[lab] = random.randint(0,255)
+			green[lab] = random.randint(0,255)
+			red[lab] = random.randint(0,255)
+	
+		# Color component pixels
+		for i in range(0,width):
+			for j in range(0, height): 
+				#if (image[j,i] != 0): # ignore black
+				lab = int(image[j,i])
+				color_image[j,i,0] = blue[lab]
+				color_image[j,i,1] = green[lab]
+				color_image[j,i,2] = red[lab]
+	
+		return segmentedImage
 
 
 # reference below
@@ -32,8 +77,6 @@ kmeans = kmeans.fit(X)
 
 # Determine labels from fitting
 labels = kmeans.predict(X)
-print(labels)
-exit()
 
 
 
