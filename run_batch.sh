ea5c6e9deaa0b3ed558c619a7b0df96f377438fd
sleach3 <sleach3@vols.utk.edu>
2019-04-29 13:50:45 -0400

    hi

--- run_batch.sh
+++ run_batch.sh
@@ -1,134 +1,134 @@
 
 
 
-mkdir output_data/set_1/
-python3 final.py image_sets/set_1_sample_grey/ ns km 5
-python3 final.py image_sets/set_1_sample_grey/ s km 5
-python3 final.py image_sets/set_1_sample_grey/ ns kmp 5 4
-python3 final.py image_sets/set_1_sample_grey/ s kmp 5 4
+#mkdir output_data/set_1/
+#python3 final_batch.py image_sets/set_1_sample_grey/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_1_sample_grey/ s km 3 10 1
+python3 final_batch.py image_sets/set_1_sample_grey/ ns kmp 4 22 3 5
+python3 final_batch.py image_sets/set_1_sample_grey/ s kmp 4 22 3 5
 
-python3 final.py image_sets/set_1_sample_grey/ ns ms 
-python3 final.py image_sets/set_1_sample_grey/ s ms 
-python3 final.py image_sets/set_1_sample_grey/ ns msp 4
-python3 final.py image_sets/set_1_sample_grey/ s msp 4
+#python3 final_batch.py image_sets/set_1_sample_grey/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_1_sample_grey/ s ms 40 120 10
+python3 final_batch.py image_sets/set_1_sample_grey/ ns msp 4 22 3 30 
+python3 final_batch.py image_sets/set_1_sample_grey/ s msp 4 22 3 40
 
 mv *.jpg output_data/set_1/
 
-mkdir output_data/set_2/
-python3 final.py image_sets/set_2_sample_blue_to_red/ ns km 6
-python3 final.py image_sets/set_2_sample_blue_to_red/ s km 6
-python3 final.py image_sets/set_2_sample_blue_to_red/ ns kmp 6 4
-python3 final.py image_sets/set_2_sample_blue_to_red/ s kmp 6 4
+#mkdir output_data/set_2/
+#python3 final_batch.py image_sets/set_2_sample_blue_to_red/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_2_sample_blue_to_red/ s km 3 10 1
+python3 final_batch.py image_sets/set_2_sample_blue_to_red/ ns kmp 4 22 3 8
+python3 final_batch.py image_sets/set_2_sample_blue_to_red/ s kmp 4 22 3 10
 
-python3 final.py image_sets/set_2_sample_blue_to_red/ ns ms 
-python3 final.py image_sets/set_2_sample_blue_to_red/ s ms 
-python3 final.py image_sets/set_2_sample_blue_to_red/ ns msp 4
-python3 final.py image_sets/set_2_sample_blue_to_red/ s msp 4
+#python3 final_batch.py image_sets/set_2_sample_blue_to_red/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_2_sample_blue_to_red/ s ms 40 120 10
+python3 final_batch.py image_sets/set_2_sample_blue_to_red/ ns msp 4 22 3 30
+python3 final_batch.py image_sets/set_2_sample_blue_to_red/ s msp 4 22 3 30
 
 mv *.jpg output_data/set_2/
 
-mkdir output_data/set_3/
-python3 final.py image_sets/set_3_sample_blue/ ns km 6
-python3 final.py image_sets/set_3_sample_blue/ s km 6
-python3 final.py image_sets/set_3_sample_blue/ ns kmp 6 4
-python3 final.py image_sets/set_3_sample_blue/ s kmp 6 4
+#mkdir output_data/set_3/
+#python3 final_batch.py image_sets/set_3_sample_blue/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_3_sample_blue/ s km 3 10 1
+python3 final_batch.py image_sets/set_3_sample_blue/ ns kmp 4 22 3 9
+python3 final_batch.py image_sets/set_3_sample_blue/ s kmp 4 22 3 6
 
-python3 final.py image_sets/set_3_sample_blue/ ns ms 
-python3 final.py image_sets/set_3_sample_blue/ s ms 
-python3 final.py image_sets/set_3_sample_blue/ ns msp 4
-python3 final.py image_sets/set_3_sample_blue/ s msp 4
+#python3 final_batch.py image_sets/set_3_sample_blue/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_3_sample_blue/ s ms 40 120 10
+python3 final_batch.py image_sets/set_3_sample_blue/ ns msp 4 22 3 30 
+python3 final_batch.py image_sets/set_3_sample_blue/ s msp 4 22 3 30
 
 mv *.jpg output_data/set_3/
 
-mkdir output_data/set_4/
-python3 final.py image_sets/set_4_sample_UTorange/ ns km 3
-python3 final.py image_sets/set_4_sample_UTorange/ s km 3
-python3 final.py image_sets/set_4_sample_UTorange/ ns kmp 3 4
-python3 final.py image_sets/set_4_sample_UTorange/ s kmp 3 4
+#mkdir output_data/set_4/
+#python3 final_batch.py image_sets/set_4_sample_UTorange/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_4_sample_UTorange/ s km 3 10 1
+python3 final_batch.py image_sets/set_4_sample_UTorange/ ns kmp 4 22 3 4
+python3 final_batch.py image_sets/set_4_sample_UTorange/ s kmp 4 22 3 4
 
-python3 final.py image_sets/set_4_sample_UTorange/ ns ms 
-python3 final.py image_sets/set_4_sample_UTorange/ s ms 
-python3 final.py image_sets/set_4_sample_UTorange/ ns msp 4
-python3 final.py image_sets/set_4_sample_UTorange/ s msp 4
+#python3 final_batch.py image_sets/set_4_sample_UTorange/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_4_sample_UTorange/ s ms 40 120 10
+python3 final_batch.py image_sets/set_4_sample_UTorange/ ns msp 4 22 3 30 
+python3 final_batch.py image_sets/set_4_sample_UTorange/ s msp 4 22 3 30
 
 mv *.jpg output_data/set_4/
 
-mkdir output_data/set_5/
-python3 final.py image_sets/set_5_sample_outfit/ ns km 5
-python3 final.py image_sets/set_5_sample_outfit/ s km 5
-python3 final.py image_sets/set_5_sample_outfit/ ns kmp 5 4
-python3 final.py image_sets/set_5_sample_outfit/ s kmp 5 4
+#mkdir output_data/set_5/
+#python3 final_batch.py image_sets/set_5_sample_outfit/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_5_sample_outfit/ s km 3 10 1
+python3 final_batch.py image_sets/set_5_sample_outfit/ ns kmp 4 22 3 9
+python3 final_batch.py image_sets/set_5_sample_outfit/ s kmp 4 22 3 9
 
-python3 final.py image_sets/set_5_sample_outfit/ ns ms 
-python3 final.py image_sets/set_5_sample_outfit/ s ms 
-python3 final.py image_sets/set_5_sample_outfit/ ns msp 4
-python3 final.py image_sets/set_5_sample_outfit/ s msp 4
+#python3 final_batch.py image_sets/set_5_sample_outfit/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_5_sample_outfit/ s ms 40 120 10
+python3 final_batch.py image_sets/set_5_sample_outfit/ ns msp 4 22 3 30 
+python3 final_batch.py image_sets/set_5_sample_outfit/ s msp 4 22 3 30
 
 mv *.jpg output_data/set_5/
 
-mkdir output_data/set_6/
-python3 final.py image_sets/set_6_sample_yellow/ ns km 3
-python3 final.py image_sets/set_6_sample_yellow/ s km 3
-python3 final.py image_sets/set_6_sample_yellow/ ns kmp 3 4
-python3 final.py image_sets/set_6_sample_yellow/ s kmp 3 4
+#mkdir output_data/set_6/
+#python3 final_batch.py image_sets/set_6_sample_yellow/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_6_sample_yellow/ s km 3 10 1
+python3 final_batch.py image_sets/set_6_sample_yellow/ ns kmp 4 22 3 4
+python3 final_batch.py image_sets/set_6_sample_yellow/ s kmp 4 22 3 4 
 
-python3 final.py image_sets/set_6_sample_yellow/ ns ms 
-python3 final.py image_sets/set_6_sample_yellow/ s ms 
-python3 final.py image_sets/set_6_sample_yellow/ ns msp 4
-python3 final.py image_sets/set_6_sample_yellow/ s msp 4
+#python3 final_batch.py image_sets/set_6_sample_yellow/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_6_sample_yellow/ s ms 40 120 10
+python3 final_batch.py image_sets/set_6_sample_yellow/ ns msp 4 22 3 30 
+python3 final_batch.py image_sets/set_6_sample_yellow/ s msp 4 22 3 30
 
 mv *.jpg output_data/set_6/
 
 
-mkdir output_data/set_7/
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ ns km 5
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ s km 5
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ ns kmp 5 4
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ s kmp 5 4
+#mkdir output_data/set_7/
+#python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ s km 3 10 1
+python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ ns kmp 4 22 3 7
+python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ s kmp 4 22 3 6
 
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ ns ms 
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ s ms 
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ ns msp 4
-python3 final.py image_sets/set_7_sample_on_clothing_logos/ s msp 4
+#python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ s ms 40 120 10
+python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ ns msp 4 22 3 50 
+python3 final_batch.py image_sets/set_7_sample_on_clothing_logos/ s msp 4 22 3 120
 
 mv *.jpg output_data/set_7/
 
-mkdir output_data/set_8/
-python3 final.py image_sets/set_8_sample_green/ ns km 3
-python3 final.py image_sets/set_8_sample_green/ s km 3
-python3 final.py image_sets/set_8_sample_green/ ns kmp 3 4
-python3 final.py image_sets/set_8_sample_green/ s kmp 3 4
+#mkdir output_data/set_8/
+#python3 final_batch.py image_sets/set_8_sample_green/ ns km 3 10 1
+#python3 final_batch.py image_sets/set_8_sample_green/ s km 3 10 1
+python3 final_batch.py image_sets/set_8_sample_green/ ns kmp 4 22 3 6
+python3 final_batch.py image_sets/set_8_sample_green/ s kmp 4 22 3 5
 
-python3 final.py image_sets/set_8_sample_green/ ns ms 
-python3 final.py image_sets/set_8_sample_green/ s ms 
-python3 final.py image_sets/set_8_sample_green/ ns msp 4
-python3 final.py image_sets/set_8_sample_green/ s msp 4
+#python3 final_batch.py image_sets/set_8_sample_green/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_8_sample_green/ s ms 40 120 10
+python3 final_batch.py image_sets/set_8_sample_green/ ns msp 4 22 3 30 
+python3 final_batch.py image_sets/set_8_sample_green/ s msp 4 22 3 30
 
 mv *.jpg output_data/set_8/
 
-mkdir output_data/set_9/
-python3 final.py image_sets/set_9_output_macbeth/ ns km 10
-python3 final.py image_sets/set_9_output_macbeth/ s km 10
-python3 final.py image_sets/set_9_output_macbeth/ ns kmp 10 4
-python3 final.py image_sets/set_9_output_macbeth/ s kmp 10 4
+#mkdir output_data/set_9/
+#python3 final_batch.py image_sets/set_9_output_macbeth/ ns km 20 30 1
+#python3 final_batch.py image_sets/set_9_output_macbeth/ s km 20 30 1
+python3 final_batch.py image_sets/set_9_output_macbeth/ ns kmp 4 22 3 10
+python3 final_batch.py image_sets/set_9_output_macbeth/ s kmp 4 22 3 10
 
-python3 final.py image_sets/set_9_output_macbeth/ ns ms 
-python3 final.py image_sets/set_9_output_macbeth/ s ms 
-python3 final.py image_sets/set_9_output_macbeth/ ns msp 4
-python3 final.py image_sets/set_9_output_macbeth/ s msp 4
+#python3 final_batch.py image_sets/set_9_output_macbeth/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_9_output_macbeth/ s ms 40 120 10
+python3 final_batch.py image_sets/set_9_output_macbeth/ ns msp4 22 3 25
+python3 final_batch.py image_sets/set_9_output_macbeth/ s msp 4 22 3 25
 
 mv *.jpg output_data/set_9/
 
-mkdir output_data/set_10/
-python3 final.py image_sets/set_10_lucy/ ns km 10
-python3 final.py image_sets/set_10_lucy/ s km 10
-python3 final.py image_sets/set_10_lucy/ ns kmp 10 4
-python3 final.py image_sets/set_10_lucy/ s kmp 10 4
+#mkdir output_data/set_10/
+#python3 final_batch.py image_sets/set_10_lucy/ ns km 20 30 1
+#python3 final_batch.py image_sets/set_10_lucy/ s km 20 30 1
+#python3 final_batch.py image_sets/set_10_lucy/ ns kmp 10 4
+#python3 final_batch.py image_sets/set_10_lucy/ s kmp 10 4
 
-python3 final.py image_sets/set_10_lucy/ ns ms 
-python3 final.py image_sets/set_10_lucy/ s ms 
-python3 final.py image_sets/set_10_lucy/ ns msp 4
-python3 final.py image_sets/set_10_lucy/ s msp 4
+#python3 final_batch.py image_sets/set_10_lucy/ ns ms 40 120 10
+#python3 final_batch.py image_sets/set_10_lucy/ s ms 40 120 10
 
-mv *.jpg output_data/set_10/
+#python3 final_batch.py image_sets/set_10_lucy/ ns msp 4
+#python3 final_batch.py image_sets/set_10_lucy/ s msp 4
+
 
