a8d7020680f9a315807d42a4b621cd45b4bd1242
Clay <sleach3@vols.utk.edu>
2019-05-01 12:31:54 -0400

    Added Comments

--- final.py
+++ final.py
@@ -5,7 +5,19 @@ import random
 
 from sklearn.datasets.samples_generator import make_blobs
 
-""" If using Mean shift, give a 3 argument if you want to use a given bandwidth, otherwise it will be estimated
+""" 
+This is the main file of the project. From this function kmeans and mean shift will be called a passed the appropriate values.
+argv[1]: the path to a folder containing multispectral images. 
+argv[2]: ns (non-spatial) or s (spatial)
+argv[3]: km (k-means), ms (meanshift), kmp (kmeans using pca), msp ( meanshift using pca)
+argv[4]: either the k value for k-means or the bandwidth for mean shift.
+argv[5]: if using pca this is the pca value.
+
+output:
+	set{}_method_spatial_k/bandwidth(_pca).jpg this is the processed multispectral output.
+	set{}_method_spatial_k/bandwidth(_pca)_c.jpg this is the processed RGB output.
+
+Both images are concatenated with the original RGB image. 
 
 """
 def main():
@@ -18,26 +30,24 @@ def main():
 	sub = elements[1].split("_")
 	
 	filename = sub[0]+sub[1]+"_"+method+"_"+spatial
+	
+	#converting spatial to a value to drop into code
 	if spatial == "s":
 		spatial = 1
 	else:
 		spatial = 0
 		
-	
+	#process data with mean shift
 	if method == "ms":
-		
+
 		multispectral = Open(imageset)
 		data =Flatten(multispectral,spatial = spatial)
 
 		color_image = MultispectralToBGR(multispectral)
 		data_c = Flatten(color_image,spatial = spatial)
 
-		if(len(sys.argv) <= 4):	
-			labels, centers, bandwidth = MultiMeanShift(data)
-			labels_c, centers_c, bandwidth_c = MultiMeanShift(data_c)
-		else:
-			labels, centers ,bandwidth= MultiMeanShift(data,int(sys.argv[4]))
-			labels_c, centers_c, bandwidth_c = MultiMeanShift(data_c,int(sys.argv[4]))
+		labels, centers ,bandwidth= MultiMeanShift(data,int(sys.argv[4]))
+		labels_c, centers_c, bandwidth_c = MultiMeanShift(data_c,int(sys.argv[4]))
 
 		
 		filename += "_"+str(int(bandwidth))
@@ -45,7 +55,7 @@ def main():
 		output = MarkupRGBImage(color_image,labels,NULL_ARR)
 		output_c = MarkupRGBImage(color_image,labels_c,NULL_ARR)
 
-
+	#process data with k means
 	elif method == "km":
 		k = int(sys.argv[4])
 		filename += "_"+str(k)
@@ -63,7 +73,7 @@ def main():
 		output = MarkupRGBImage(color_image, labels, NULL_ARR)
 		output_c = MarkupRGBImage(color_image, labels_c, NULL_ARR)
 	
-	
+	#process data with mean shift and PCA
 	elif method == "msp":
 		
 		multispectral = Open(imageset)
@@ -73,21 +83,16 @@ def main():
 		data_c =Flatten(color_image,spatial=spatial)
 		
 		
-		if(len(sys.argv) <= 5):	
-			pca = int(sys.argv[4])
-			data = PCAReduction(pca,data)
-			labels, centers, bandwidth = MultiMeanShift(data)
-			labels_c, centers_c, bandwidth_c = MultiMeanShift(data_c)
-		else:
-			pca = int(sys.argv[6])
-			data = PCAReduction(pca,data)
-			labels, centers ,bandwidth= MultiMeanShift(data,int(sys.argv[4]))
-			labels_c, centers_c, bandwidth_c = MultiMeanShift(data_c,int(sys.argv[4]))
+		pca = int(sys.argv[6])
+		data = PCAReduction(pca,data)
+		labels, centers ,bandwidth= MultiMeanShift(data,int(sys.argv[4]))
+		labels_c, centers_c, bandwidth_c = MultiMeanShift(data_c,int(sys.argv[4]))
 
 		filename += "_"+str(int(bandwidth))+"_"+str(pca)
 		
 		output = MarkupRGBImage(color_image,labels,NULL_ARR)
 		output_c = MarkupRGBImage(color_image,labels_c,NULL_ARR)
+	#process data with k-means and PCA
 	elif method == "kmp":
 		k = int(sys.argv[4])
 		pca = int(sys.argv[5])
@@ -108,25 +113,6 @@ def main():
 
 		output = MarkupRGBImage(color_image, labels, NULL_ARR)
 		output_c = MarkupRGBImage(color_image, labels_c, NULL_ARR)
-	elif method == "test":
-		k = int(sys.argv[4])
-
-		#multispectral = Open(imageset)
-		color_image = cv.imread(imageset,1)#MultispectralToBGR(multispectral)
-
-		#data = Flatten(multispectral,spatial=spatial)	
-		data = Flatten(color_image,spatial=spatial)
-
-
-		labels, centers = MultiKMeans(k, data)
-		#labels, centers, bandwidth = MultiMeanShift(data,k)
-		#labels_c, centers_c = MultiKMeans(k, data_c)
-
-		output = MarkupRGBImage(color_image, labels, NULL_ARR)
-		#output_c = MarkupRGBImage(color_image, labels_c, NULL_ARR)
-		cv.imshow("test",output)
-		cv.waitKey(0)
-		exit()
 	
 	filename_c = filename + "_c.jpg"
 	filename +=".jpg"
